377935a64d5b1c6eaac81e42f83739f6dbb06ca9
taiminhlee <pck14451@gmail.com>
2021-11-18 11:03:51 +0700

    upbai

--- untitled/src/ss11_dsa_stack_queue/dem_so_lan_xuat_hien_cua_moi_tu_trong_mot_chuoi_su_dung_map/DemSoLanXuatHien.java
+++ untitled/src/ss11_dsa_stack_queue/dem_so_lan_xuat_hien_cua_moi_tu_trong_mot_chuoi_su_dung_map/DemSoLanXuatHien.java
@@ -1,7 +1,6 @@
 package ss11_dsa_stack_queue.dem_so_lan_xuat_hien_cua_moi_tu_trong_mot_chuoi_su_dung_map;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 public class DemSoLanXuatHien {
@@ -10,25 +9,19 @@ public class DemSoLanXuatHien {
         str = str.replaceAll(" ", "");
         str = str.trim();
         String[] arr = str.toLowerCase().split("");
-        String key;
-        Integer value;
-        Map<String, Integer> map = new TreeMap<>();
+        Map<String,Integer > map = new TreeMap<>();
+        Integer temp;
         for (String s : arr) {
-            key = s;
-            if (map.containsKey(key)) {
-                value = map.get(key);
-                map.remove(key);
-                map.put(key, value + 1);
+            if (map.containsKey(s)) {
+                temp = map.get(s);
+                map.remove(s);
+                map.put(s,temp + 1);
             } else {
-                map.put(key, 1);
+                map.put(s,1);
             }
         }
-
-        Set set=map.keySet();
-
-        for (Object o : set) {
-            key = (String) o;
-            System.out.println("Từ '" + key + "' xuất hiện " + map.get(key) + " lần");
+        for (String key1 : map.keySet()) {
+            System.out.println("Từ '" + key1 + "' xuất hiện " + map.get(key1) + " lần");
         }
     }
 }
